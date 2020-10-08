1e89d495dbf5bfe248b2b25b6f1bf10ecce87f08
Jann Mueller <jann.mueller@iohk.io>
2020-10-08 16:03:46 +0200

    System threads

--- plutus-contract/src/Plutus/Trace/Emulator/System.hs
+++ plutus-contract/src/Plutus/Trace/Emulator/System.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE DataKinds           #-}
 {-# LANGUAGE FlexibleContexts    #-}
@@ -9,24 +10,30 @@ module Plutus.Trace.Emulator.System(
     launchSystemThreads
     ) where
 
-import           Control.Monad                 (forever)
+import           Control.Monad                 (forever, forM_)
 import           Control.Monad.Freer
+import Data.Foldable (traverse_)
 import           Control.Monad.Freer.Coroutine
 import           Wallet.Emulator.Chain         (ChainControlEffect, ChainEffect, getCurrentSlot, processBlock)
+import           Wallet.Emulator.MultiAgent                    (MultiAgentEffect, walletControlAction)
 
 import           Plutus.Trace.Emulator.Types   (EmulatorEvent (..))
-import           Plutus.Trace.Scheduler        (Priority (..), SysCall (..), SystemCall, fork, mkSysCall)
+import           Plutus.Trace.Scheduler        (Priority (..), SysCall (..), SystemCall, fork, mkSysCall, sleep)
+import           Wallet.Emulator.Wallet                        (Wallet(..))
+import Wallet.Emulator.ChainIndex (chainIndexNotify)
+import Wallet.Emulator.NodeClient (clientNotify, ChainClientNotification(..))
 
 launchSystemThreads :: forall effs.
     ( Member ChainControlEffect effs
+    , Member MultiAgentEffect effs
     , Member ChainEffect effs
     )
     => Eff (Yield (SystemCall effs EmulatorEvent) (Maybe EmulatorEvent) ': effs) ()
 launchSystemThreads = do
     -- 1. Block maker
     _ <- fork @effs @EmulatorEvent Low (blockMaker @effs)
-    -- 2. TODO: Threads for updating the agents' states
-    pure ()
+    -- 2. Threads for updating the agents' states
+    traverse_ (fork @effs @EmulatorEvent Low . agentThread @effs) (Wallet <$> [1..10])
 
 blockMaker :: forall effs effs2.
     ( Member ChainControlEffect effs2
@@ -41,4 +48,22 @@ blockMaker = forever go where
         newSlot <- getCurrentSlot
         mkSysCall @effs Sleeping (Broadcast $ NewSlot newSlot)
 
+agentThread :: forall effs effs2.
+    ( Member MultiAgentEffect effs2
+    , Member (Yield (SystemCall effs EmulatorEvent) (Maybe EmulatorEvent)) effs2
+    )
+    => Wallet
+    -> Eff effs2 ()
+agentThread wllt = forever go where
+    go = do
+        e <- sleep @effs @EmulatorEvent Sleeping
+        let noti = e >>= \case
+                BlockAdded block -> Just $ BlockValidated block
+                NewSlot slot -> Just $ SlotChanged slot
+                _ -> Nothing
+
+        forM_ noti $ \n -> do
+            walletControlAction wllt $ do
+                clientNotify n
+                chainIndexNotify n
 
