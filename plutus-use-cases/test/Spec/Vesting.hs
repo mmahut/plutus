1d3f500afae8008779b4286d15545cf9b8636f2d
Jann Mueller <jann.mueller@iohk.io>
2020-11-09 10:11:29 +0100

    Two more contracts

--- plutus-use-cases/test/Spec/Vesting.hs
+++ plutus-use-cases/test/Spec/Vesting.hs
@@ -9,6 +9,7 @@
 {-# OPTIONS_GHC -fno-warn-incomplete-uni-patterns -fno-warn-unused-do-bind #-}
 module Spec.Vesting(tests, retrieveFundsTrace, vesting) where
 
+import Control.Monad (void)
 import           Test.Tasty
 import qualified Test.Tasty.HUnit                                 as HUnit
 
@@ -22,56 +23,52 @@ import qualified Ledger
 import qualified Ledger.Ada                                       as Ada
 
 import           Language.Plutus.Contract.Test
+import qualified Plutus.Trace.Emulator    as Trace
+import Plutus.Trace.Emulator (EmulatorTrace)
 import           Prelude                                          hiding (not)
 
-wallet1, wallet2 :: Wallet
-wallet1 = Wallet 1
-wallet2 = Wallet 2
+w1, w2 :: Wallet
+w1 = Wallet 1
+w2 = Wallet 2
 
 tests :: TestTree
 tests =
     let con = vestingContract vesting in
     testGroup "vesting"
-    [ checkPredicate "secure some funds with the vesting script"
-        con
-        (walletFundsChange wallet2 (Numeric.negate $ totalAmount vesting))
-        ( callEndpoint @"vest funds" wallet2 ()
-        >> handleBlockchainEvents wallet2
-        >> addBlocks 1
-        )
+    [ checkPredicate defaultCheckOptions "secure some funds with the vesting script"
+        (walletFundsChange w2 (Numeric.negate $ totalAmount vesting))
+        $ do
+            hdl <- Trace.activateContractWallet w2 con
+            Trace.callEndpoint @"vest funds" w2 hdl ()
 
-    , checkPredicate "retrieve some funds"
-        con
-        (walletFundsChange wallet2 (Numeric.negate $ totalAmount vesting)
-        /\ assertNoFailedTransactions
-        /\ walletFundsChange wallet1 (Ada.lovelaceValueOf 10))
+    , checkPredicate defaultCheckOptions "retrieve some funds"
+        (walletFundsChange w2 (Numeric.negate $ totalAmount vesting)
+        .&&. assertNoFailedTransactions
+        .&&. walletFundsChange w1 (Ada.lovelaceValueOf 10))
         retrieveFundsTrace
 
-    , checkPredicate "cannot retrieve more than allowed"
-        con
-        (walletFundsChange wallet1 mempty
-        /\ assertContractError wallet1 (== expectedError) "error should match")
-        (callEndpoint @"vest funds" wallet2 ()
-        >> handleBlockchainEvents wallet2
-        >> addBlocks 10
-        >> callEndpoint @"retrieve funds" wallet1 (Ada.lovelaceValueOf 30)
-        >> notifySlot wallet1
-        >> addBlocks 1
-        >> handleBlockchainEvents wallet1)
+    , checkPredicate defaultCheckOptions "cannot retrieve more than allowed"
+        (walletFundsChange w1 mempty
+        .&&. assertContractError con (Trace.walletInstanceTag w1) (== expectedError) "error should match")
+        $ do
+            hdl1 <- Trace.activateContractWallet w1 con
+            hdl2 <- Trace.activateContractWallet w2 con
+            Trace.callEndpoint @"vest funds" w2 hdl2 ()
+            Trace.waitNSlots 10
+            Trace.callEndpoint @"retrieve funds" w1 hdl1 (Ada.lovelaceValueOf 30)
+            void $ Trace.waitNSlots 1
 
-    , checkPredicate "can retrieve everything at the end"
-        con
-        (walletFundsChange wallet1 (totalAmount vesting)
-        /\ assertNoFailedTransactions
-        /\ assertDone wallet1 (const True) "should be done")
-        (callEndpoint @"vest funds" wallet2 ()
-        >> handleBlockchainEvents wallet2
-        >> addBlocks 20
-        >> callEndpoint @"retrieve funds" wallet1 (totalAmount vesting)
-        >> notifySlot wallet1
-        >> handleBlockchainEvents wallet1
-        >> addBlocks 1
-        )
+    , checkPredicate defaultCheckOptions "can retrieve everything at the end"
+        (walletFundsChange w1 (totalAmount vesting)
+        .&&. assertNoFailedTransactions
+        .&&. assertDone con (Trace.walletInstanceTag w1) (const True) "should be done")
+        $ do
+            hdl1 <- Trace.activateContractWallet w1 con
+            hdl2 <- Trace.activateContractWallet w2 con
+            Trace.callEndpoint @"vest funds" w2 hdl2 ()
+            Trace.waitNSlots 20
+            Trace.callEndpoint @"retrieve funds" w1 hdl1 (totalAmount vesting)
+            void $ Trace.waitNSlots 1
 
     , Lib.goldenPir "test/Spec/vesting.pir" $$(PlutusTx.compile [|| validate ||])
     , HUnit.testCase "script size is reasonable" (Lib.reasonable (vestingScript vesting) 33000)
@@ -85,26 +82,21 @@ vesting =
     VestingParams
         { vestingTranche1 = VestingTranche (Ledger.Slot 10) (Ada.lovelaceValueOf 20)
         , vestingTranche2 = VestingTranche (Ledger.Slot 20) (Ada.lovelaceValueOf 40)
-        , vestingOwner    = Ledger.pubKeyHash $ walletPubKey wallet1 }
+        , vestingOwner    = Ledger.pubKeyHash $ walletPubKey w1 }
 
-retrieveFundsTrace
-    :: ContractTrace VestingSchema e () ()
+retrieveFundsTrace :: EmulatorTrace ()
 retrieveFundsTrace = do
-    callEndpoint @"vest funds" wallet2 ()
-    handleBlockchainEvents wallet2
-    addBlocks 10
-    callEndpoint @"retrieve funds" wallet1 (Ada.lovelaceValueOf 10)
-    addBlocks 1
-    handleBlockchainEvents wallet1
-    addBlocks 1
+    let con = vestingContract @VestingError vesting
+    hdl1 <- Trace.activateContractWallet w1 con
+    hdl2 <- Trace.activateContractWallet w2 con
+    Trace.callEndpoint @"vest funds" w2 hdl2 ()
+    Trace.waitNSlots 10
+    Trace.callEndpoint @"retrieve funds" w1 hdl1 (Ada.lovelaceValueOf 10)
+    void $ Trace.waitNSlots 1
 
-expectedError :: TraceError VestingError
+expectedError :: VestingError
 expectedError =
     let payment = Ada.lovelaceValueOf 30
         maxPayment = Ada.lovelaceValueOf 20
         mustRemainLocked = Ada.lovelaceValueOf 40
-    in TContractError
-        $ InsufficientFundsError
-            payment
-            maxPayment
-            mustRemainLocked
+    in InsufficientFundsError payment maxPayment mustRemainLocked
